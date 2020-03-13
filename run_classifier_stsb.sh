469a2fec4e567ca44bbdbcafb6a0adf5bdb99ea0
lonePatinet <1436496575@qq.com>
2020-03-13 10:13:06 +0800

    Update run_classifier_stsb.sh

--- run_classifier_stsb.sh
+++ run_classifier_stsb.sh
@@ -1,5 +1,5 @@
 CURRENT_DIR=`pwd`
-export BERT_BASE_DIR=$CURRENT_DIR/prev_trained_model/electra_base
+export BERT_BASE_DIR=$CURRENT_DIR/prev_trained_model/electra_small
 export DATA_DIR=$CURRENT_DIR/dataset
 export OUTPUR_DIR=$CURRENT_DIR/outputs
 TASK_NAME="sts-b"
@@ -22,4 +22,52 @@ python run_classifier.py \
   --overwrite_output_dir \
   --seed=42
 
-#samll 1e-4
+
+CURRENT_DIR=`pwd`
+export BERT_BASE_DIR=$CURRENT_DIR/prev_trained_model/electra_base
+export DATA_DIR=$CURRENT_DIR/dataset
+export OUTPUR_DIR=$CURRENT_DIR/outputs
+TASK_NAME="sts-b"
+python run_classifier.py \
+  --model_type=bert \
+  --model_name_or_path=$BERT_BASE_DIR \
+  --task_name=$TASK_NAME \
+  --do_train \
+  --do_eval \
+  --do_lower_case \
+  --data_dir=$DATA_DIR/${TASK_NAME}/ \
+  --max_seq_length=128 \
+  --per_gpu_train_batch_size=16 \
+  --per_gpu_eval_batch_size=16 \
+  --learning_rate=5e-5 \
+  --num_train_epochs=3.0 \
+  --logging_steps=360 \
+  --save_steps=360 \
+  --output_dir=$OUTPUR_DIR/${TASK_NAME}_output/ \
+  --overwrite_output_dir \
+  --seed=42
+
+#
+CURRENT_DIR=`pwd`
+export BERT_BASE_DIR=$CURRENT_DIR/prev_trained_model/electra_large
+export DATA_DIR=$CURRENT_DIR/dataset
+export OUTPUR_DIR=$CURRENT_DIR/outputs
+TASK_NAME="sts-b"
+python run_classifier.py \
+  --model_type=bert \
+  --model_name_or_path=$BERT_BASE_DIR \
+  --task_name=$TASK_NAME \
+  --do_train \
+  --do_eval \
+  --do_lower_case \
+  --data_dir=$DATA_DIR/${TASK_NAME}/ \
+  --max_seq_length=128 \
+  --per_gpu_train_batch_size=8 \
+  --per_gpu_eval_batch_size=16 \
+  --learning_rate=1e-5 \
+  --num_train_epochs=3.0 \
+  --logging_steps=360 \
+  --save_steps=360 \
+  --output_dir=$OUTPUR_DIR/${TASK_NAME}_output/ \
+  --overwrite_output_dir \
+  --seed=42
